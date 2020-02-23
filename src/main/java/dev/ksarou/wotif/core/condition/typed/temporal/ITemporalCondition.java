22e96d59671259c8d305ee45294631b57725e643
cdepadua <cesaredepadua@gmail.com>
2020-02-23 16:12:21 +0100

    Create an internal temporal conditions file

--- src/main/java/dev/ksarou/wotif/core/condition/typed/temporal/ITemporalCondition.java
+++ src/main/java/dev/ksarou/wotif/core/condition/typed/temporal/ITemporalCondition.java
@@ -1,17 +1,34 @@
-package dev.ksarou.wotif.core.condition.typed.time;
+package dev.ksarou.wotif.core.condition.typed.temporal;
 
 import dev.ksarou.wotif.core.Completable;
 import dev.ksarou.wotif.core.condition.typed.IComparableCondition;
 
-public interface ITimeCondition<T extends Comparable<T>, R>
+public interface ITemporalCondition<T extends Comparable<T>, R>
         extends IComparableCondition<T, R> {
 
     Completable<R> isAfter(String otherAsString);
+
     Completable<R> isAfter(T other);
+
     Completable<R> isAfterOrEqualTo(String otherAsString);
+
     Completable<R> isAfterOrEqualTo(T other);
+
     Completable<R> isBefore(String otherAsString);
+
     Completable<R> isBefore(T other);
+
     Completable<R> isBeforeOrEqualTo(String otherAsString);
+
     Completable<R> isBeforeOrEqualTo(T other);
+
+    Completable<R> isBetween(String inclusiveStart, String inclusiveEnd);
+
+    Completable<R> isBetween(T inclusiveStart, T inclusiveEnd);
+
+    Completable<R> isStrictlyBetween(String inclusiveStart, String inclusiveEnd);
+
+    Completable<R> isStrictlyBetween(T inclusiveStart, T inclusiveEnd);
+
+    Completable<R> isToday();
 }
--- src/main/java/dev/ksarou/wotif/core/condition/typed/temporal/InternalTemporalCondition.java
+++ src/main/java/dev/ksarou/wotif/core/condition/typed/temporal/InternalTemporalCondition.java
@@ -1,4 +1,36 @@
 package dev.ksarou.wotif.core.condition.typed.temporal;
 
+import java.time.LocalDateTime;
+
 public class InternalTemporalCondition {
+
+    LocalDateTime localDateTime;
+
+    public boolean isAfter(LocalDateTime other) {
+        return this.localDateTime.isAfter(other);
+    }
+
+    public boolean isAfterOrEqualTo(LocalDateTime other) {
+        return this.localDateTime.isAfter(other) || this.localDateTime.isEqual(other);
+    }
+
+    public boolean isBefore(LocalDateTime other) {
+        return this.localDateTime.isBefore(other);
+    }
+
+    public boolean isBeforeOrEqualTo(LocalDateTime other) {
+        return this.localDateTime.isBefore(other) || this.localDateTime.isEqual(other);
+    }
+
+    public boolean isBetween(LocalDateTime inclusiveStart, LocalDateTime inclusiveEnd) {
+        return false;
+    }
+
+    public boolean isStrictlyBetween(LocalDateTime inclusiveStart, LocalDateTime inclusiveEnd) {
+        return false;
+    }
+
+    public boolean isToday() {
+        return false;
+    }
 }
